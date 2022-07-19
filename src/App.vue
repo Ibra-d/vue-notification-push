6714946afda2217892d7db884943ae6cde675574
DRAME-ibrahima <drame.ibou.07@gmail.com>
2022-07-19 09:57:29 +0200

    Notif-pusher

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <Notif />
   </div>
 </template>
@@ -17,12 +16,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
 }
 </style>
--- src/components/Notif.vue
+++ src/components/Notif.vue
@@ -1,58 +1,63 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="Notif">
+    <div class="parent_notif_button">
+      <button class="button_to_notif" @click="NotifPush">
+        Recevoir Notification
+      </button>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+<script setup>
+const NotifPush = function () {
+  Notification.requestPermission()
+    .then(function (result) {
+      if (result === "granted") {
+        console.log(result);
+      }
+      const img = "https://cdn-icons-png.flaticon.com/800/7207/7207402.png";
+      const text = "Coucou ! Votre commance est en cours de preparation.";
+      const notification = new Notification("Commande Reçu !", {
+        body: text,
+        icon: img,
+      });
+      notification.addEventListener("click", function () {
+        alert("YES");
+      });
+    })
+    .catch(function (result) {
+      console.log(result);
+    });
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.Notif {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.parent_notif_button {
+  height: 5rem;
+  width: 15rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.5rem;
+  box-shadow: #c1c1ff 0px 8px 24px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.button_to_notif {
+  border: 1px solid #c1c1ff;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  border-radius: 0.5rem;
+  transition: 0.1s linear;
+  font-family: Monospace;
 }
-a {
-  color: #42b983;
+.button_to_notif:hover {
+  background-color: #bcbcbc;
 }
 </style>
